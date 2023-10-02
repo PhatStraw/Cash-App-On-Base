57a81790d7da50a4caaef604fb540f28b18aae1d
PhatStraws <kevindsims1@gmail.com>
2023-10-01 21:06:23 -0700

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from 'components/styles/Home.module.css'
 import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { BigNumber, ethers } from 'ethers'
 import abi from "../abi.json"
 import logo from "../public/logo.png"
 import Action from 'components/components/action'
-const inter = Inter({ subsets: ['latin'] })
-const user = {
-  name: "kevin",
-  email: "kev@gmail.com"
-}
 export default function Home() {
   const [signer, setSigner] = useState(null)
   const [provider, setProvider] = useState(null)
@@ -28,7 +18,10 @@ export default function Home() {
   const [from, setFrom] = useState()
   const [amount, setAmount] = useState()
   const [withdrawl, setWithdrawl] = useState()
+  const [balance, setBalance] = useState()
+  const [network, setNetwork] = useState()
 
+  //Connect Wallet
   useEffect(() => {
     const connectWallet = async () => {
       setLoading(true)
@@ -43,34 +36,51 @@ export default function Home() {
         setProvider(pro)
         setLoading(false)
       } else {
-        // A Web3Provider wraps a standard Web3 provider, which is
-        // what MetaMask injects as window.ethereum into each page
-        const pro = new ethers.providers.Web3Provider(window.ethereum)
-        setProvider(pro)
-        // MetaMask requires requesting permission to connect users accounts
-        await pro.send("eth_requestAccounts", []);
-        // The MetaMask plugin also allows signing transactions to
-        // send ether and pay to change state within the blockchain.
-        // For this, you need the account signer...
-        const sign = pro.getSigner()
-        setSigner(sign)
-        // The Contract object
-        const CashAppContract = new ethers.Contract(abi.address, abi.abi, pro)
-        setContract(CashAppContract)
-        //Last Message Sent
-        const messy = await CashAppContract.messages("0")
-        setMessages((prevMessages) => [...prevMessages, { content: messy[0], sender: messy[1], receiver: messy[2] }])
+        try {
+          // A Web3Provider wraps a standard Web3 provider, which is
+          // what MetaMask injects as window.ethereum into each page
+          const pro = new ethers.providers.Web3Provider(window.ethereum)
+          setProvider(pro)
 
-        //Signers CashTag
-        const cashTag = await CashAppContract.addressToCashtag(await sign.getAddress())
-        setCashTag(cashTag)
+          // MetaMask requires requesting permission to connect users accounts
+          await pro.send("eth_requestAccounts", []);
+
+          // The MetaMask plugin also allows signing transactions to
+          // send ether and pay to change state within the blockchain.
+          // For this, you need the account signer...
+          const sign = pro.getSigner()
+          setSigner(sign)
+
+          // The Contract object
+          const CashAppContract = new ethers.Contract(abi.address, abi.abi, pro)
+          setContract(CashAppContract)
+
+          //Last Message Sent
+          const messy = await CashAppContract.messages("0")
+          setMessages((prevMessages) => [...prevMessages, { content: messy[0], sender: messy[1], receiver: messy[2] }])
+
+          //Signers CashTag and balance
+          const cashTag = await CashAppContract.addressToCashtag(await sign.getAddress())
+          const balance = await CashAppContract.balances(cashTag)
+          const ether = BigNumber.from(balance)
+          const eth = await ethers.utils.formatUnits(ether.toString(), 18);
+          setBalance(eth)
+          setCashTag(cashTag)
+        } catch (error) {
+          console.log(error)
+          alert(error)
+        }
         setLoading(false)
+        // Clean up the event listener when the component unmounts
+        // return () => {
+        //   provider.off("network");
+        // };
       }
     }
     connectWallet()
-
   }, [])
 
+  //Track MessageSent Events
   useEffect(() => {
     const trythis = async () => {
       if (contract) {
@@ -88,6 +98,29 @@ export default function Home() {
     };
   }, [contract]);
 
+  //Track network changes
+  useEffect(() => {
+    const externalProvider = window.ethereum; // Your existing Web3-compatible provider
+    const provider = new ethers.providers.Web3Provider(externalProvider);
+  
+    provider.on("network", (newNetwork, oldNetwork) => {
+      if(!oldNetwork && newNetwork.chainId !== 84531){
+        alert("APP WILL NOT WORK: Change Your Network TO Base GOERLI")
+      }
+      if (oldNetwork) {
+        // Network has changed, refresh the page to reset UI components
+        window.location.reload();
+      }
+    });
+  
+    // Your existing code...
+  
+    // Clean up the event listener when the component unmounts
+    return () => {
+      provider.off("network");
+    };
+  }, []);
+
   const onRegister = async () => {
     if (register) {
       // Contract is currently connected to the Provider,
@@ -102,7 +135,6 @@ export default function Home() {
       setLoading(false)
     }
   }
-
 
   const onPay = async () => {
     if (to && message) {
@@ -140,6 +172,9 @@ export default function Home() {
         <div>
           {cashTag ? `Cash Tag: ${cashTag}` : "No CashTag Found For This Wallet"}
         </div>
+        <div>
+          {balance ? `Balance: ${balance}E` : "No Balance Found For This Wallet"}
+        </div>
       </div>
       {!loading ?
         <div className='grid sm:grid-cols-1 md:grid-cols-2 justify-items-center w-full md:px-[10rem]'>
@@ -149,7 +184,7 @@ export default function Home() {
               type="text"
               name="register"
               id="register"
-              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 py-1.5 pl-7 m-1 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Register a CashTag"
               onChange={e => setRegister(e.target.value)}
             />
@@ -158,10 +193,18 @@ export default function Home() {
           <Action>
             <h3 className='text-3xl align-center'>Pay</h3>
 
-            <input placeholder='To' onChange={e => setTo(e.target.value)} />
-            <input placeholder='From' onChange={e => setFrom(e.target.value)} />
-            <input placeholder='Message' onChange={e => setMessage(e.target.value)} />
-            <input placeholder='Amount' onChange={e => setAmount(e.target.value)} />
+            <input placeholder='To'
+              className="block w-full rounded-md border-0 py-1.5 pl-7 m-1 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={e => setTo(e.target.value)} />
+            <input placeholder='From'
+              className="block w-full rounded-md border-0 py-1.5 pl-7 m-1 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={e => setFrom(e.target.value)} />
+            <input placeholder='Message'
+              className="block w-full rounded-md border-0 py-1.5 pl-7 m-1 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={e => setMessage(e.target.value)} />
+            <input placeholder='Amount'
+              className="block w-full rounded-md border-0 py-1.5 pl-7 m-1 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={e => setAmount(e.target.value)} />
             <button onClick={onPay} className='bg-black text-white p-4 mt-2' >Click2Pay</button>
           </Action>
           <Action>
@@ -181,7 +224,7 @@ export default function Home() {
               ))}
             </div>
           </Action>
-        </div> : <>transactions processing...</>}
+        </div> : <div className='text-center flex justify-center items-center'>transactions processing...</div>}
     </div>
   )
 }
