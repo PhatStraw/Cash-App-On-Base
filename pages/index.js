db5333c4e0f781096d790f0a22775403826e2cdb
PhatStraw <kevindsimsjr@gmail.com>
2023-11-02 00:06:11 -0700

    accounts and refactor

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { BigNumber, ethers } from "ethers";
 import abi from "../abi.json";
-import logo from "../public/logo.png";
 import Action from "components/components/action";
 
 const Tabs = {
@@ -31,7 +29,7 @@ const TabbedActions = ({ tabs, activeTab }) => {
   );
 };
 
-const TabButtons = ({ buttons, activeButton, setActiveButton }) => {
+const TabButtons = ({ buttons, activeButton }) => {
   return (
     <div className="flex flex-wrap justify-center">
       {buttons.map((button) => (
@@ -401,12 +399,6 @@ export default function Home() {
       {network === liveNetwork ? (
         <div className="mx-auto p-0">
           <div className="text-center">
-            {/* <Image
-              key="logo"
-              alt="Pic of logo"
-              src={logo}
-              className="mx-auto h-12 w-auto"
-            /> */}
             <h1 className="py-3 text-4xl font-bold bg-indigo-600 text-white">
               Cash App on Base
             </h1>
@@ -417,7 +409,6 @@ export default function Home() {
                 <TabButtons
                   buttons={buttons}
                   activeButton={activeButton}
-                  setActiveButton={setActiveButton}
                 />
                 <TabbedActions activeTab={activeTab} tabs={tabs} />
               </div>
@@ -445,232 +436,4 @@ export default function Home() {
       )}
     </div>
   );
-}
-
-// export default function Home() {
-
-//   //Connect Wallet
-//   useEffect(() => {
-//     const connectWallet = async () => {
-//       setLoading(true)
-//       if (window.ethereum == null) {
-//         // If MetaMask is not installed, we use the default provider,
-//         // which is backed by a variety of third-party services (such
-//         // as INFURA). They do not have private keys installed so are
-//         // only have read-only access
-//         alert("MetaMask not installed; using read-only defaults")
-//         console.log("MetaMask not installed; using read-only defaults")
-//         const pro = ethers.getDefaultProvider()
-//         setProvider(pro)
-//         setLoading(false)
-//       } else {
-//         try {
-//           // A Web3Provider wraps a standard Web3 provider, which is
-//           // what MetaMask injects as window.ethereum into each page
-//           const pro = new ethers.providers.Web3Provider(window.ethereum)
-//           setProvider(pro)
-//           const { chainId } = await pro.getNetwork()
-//           setNetwork(chainId)
-//           // MetaMask requires requesting permission to connect users accounts
-//           await pro.send("eth_requestAccounts", []);
-
-//           // The MetaMask plugin also allows signing transactions to
-//           // send ether and pay to change state within the blockchain.
-//           // For this, you need the account signer...
-//           const sign = pro.getSigner()
-//           setSigner(sign)
-
-//           // The Contract object
-//           const CashAppContract = new ethers.Contract(abi.address, abi.abi, pro)
-//           setContract(CashAppContract)
-//           console.log
-//           //Last Message Sent
-//           const messy = await CashAppContract.messages("0")
-//           setMessages((prevMessages) => [...prevMessages, { content: messy[0], sender: messy[1], receiver: messy[2] }])
-
-//           //Signers CashTag and balance
-//           const cashTag = await CashAppContract.addressToCashtag(await sign.getAddress())
-//           const balance = await CashAppContract.balances(cashTag)
-//           const ether = BigNumber.from(balance)
-//           const eth = await ethers.utils.formatUnits(ether.toString(), 18);
-//           setBalance(eth)
-//           setCashTag(cashTag)
-//         } catch (error) {
-//           console.log(error)
-//           alert(error)
-//         }
-//         setLoading(false)
-//         // Clean up the event listener when the component unmounts
-//         // return () => {
-//         //   provider.off("network");
-//         // };
-//       }
-//     }
-//     connectWallet()
-//   }, [])
-
-//   //Track MessageSent Events
-//   useEffect(() => {
-//     const trythis = async () => {
-//       if (contract) {
-//         contract.on("MessageSent", (content, sender, receiver) => {
-//           console.log(`${sender} sent ${content} to ${receiver}`);
-//           setMessages((prevMessages) => [...prevMessages, { content, sender, receiver }])
-//         });
-//       }
-//     }
-//     trythis()
-//     return () => {
-//       if (contract) {
-//         contract.removeAllListeners();
-//       }
-//     };
-//   }, [contract]);
-
-//   //Track network changes
-//   useEffect(() => {
-//     const externalProvider = window.ethereum; // Your existing Web3-compatible provider
-//     const provider = new ethers.providers.Web3Provider(externalProvider);
-
-//     provider.on("network", (newNetwork, oldNetwork) => {
-//       if (!oldNetwork && newNetwork.chainId !== 84531) {
-//         alert("APP WILL NOT WORK: Change Your Network TO Base GOERLI")
-//       }
-//       if (oldNetwork) {
-//         // Network has changed, refresh the page to reset UI components
-//         window.location.reload();
-//       }
-//     });
-
-//     // Clean up the event listener when the component unmounts
-//     return () => {
-//       provider.off("network");
-//     };
-//   }, []);
-
-//   const onRegister = async () => {
-//     if (register) {
-//       // Contract is currently connected to the Provider,
-//       // which is read-only. You need to connect to a Signer, so
-//       // that you can pay to send state-changing transactions.
-//       const CashAppWithSigner = await contract.connect(signer);
-
-//       const tx = await CashAppWithSigner.register(register);
-//       setLoading(true)
-//       const wait = await tx.wait()
-//       console.log(wait)
-//       setLoading(false)
-//     }
-//   }
-
-//   const onPay = async () => {
-//     if (to && message) {
-//       // Contract is currently connected to the Provider,
-//       // which is read-only. You need to connect to a Signer, so
-//       // that you can pay to send state-changing transactions.
-//       const CashAppWithSigner = await contract.connect(signer);
-
-//       // Eth has 18 decimal places
-//       const eth = ethers.utils.parseUnits(amount, 18);
-
-//       const tx = await CashAppWithSigner.pay(to, from, message, { value: eth });
-//       setLoading(true)
-//       const wait = await tx.wait()
-//       console.log(wait)
-//       setLoading(false)
-//     }
-//   }
-
-//   const onWithdrawl = async () => {
-//     // try{
-//     if (withdrawl) {
-//       const CashAppWithSigner = await contract.connect(signer);
-//       const tx = await CashAppWithSigner.withdrawl(withdrawl);
-//       setLoading(true)
-//       const wait = await tx.wait()
-//       setLoading(false)
-//     }
-//   }
-
-//   return (
-//     <div className="min-h-screen bg-gray-100 p-6">
-//       <Head>
-//         <title>Decentralized Cash App</title>
-//         <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet" />
-//       </Head>
-//       {network === liveNetwork ?
-//         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-//           <div className="text-center">
-//             <Image key="logo" alt="Pic of logo" src={logo} className="mx-auto h-12 w-auto" />
-//             <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
-//               {cashTag ? `Cash Tag: ${cashTag}` : "No CashTag Found For This Wallet"}
-//             </h2>
-//             <p className="mt-2 text-sm text-gray-600">
-//               {balance ? `Balance: ${balance}E` : "No Balance Found For This Wallet"}
-//             </p>
-//           </div>
-//           <div className="mt-8">
-//             {!loading ?
-//               <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-//                 <Action className="bg-white p-6 rounded-lg shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-//                   <h3 className='text-xl font-bold text-center mb-4'>Register</h1>
-//                   <input
-//                     type="text"
-//                     name="register"
-//                     id="register"
-//                     className="block w-full rounded-md border-gray-300 shadow-sm mb-4 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     placeholder="Register a CashTag"
-//                     onChange={e => setRegister(e.target.value)}
-//                   />
-//                   <button onClick={onRegister} className='w-full bg-indigo-600 text-white p-3 rounded hover:bg-indigo-700 transition duration-300'>Register</button>
-//                 </Action>
-//                 <Action className="bg-white p-6 rounded-lg shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-//                   <h3 className='text-xl font-bold text-center mb-4'>Pay</h1>
-//                   <input placeholder='To'
-//                     className="block w-full rounded-md border-gray-300 shadow-sm mb-4 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     onChange={e => setTo(e.target.value)} />
-//                   <input placeholder='From'
-//                     className="block w-full rounded-md border-gray-300 shadow-sm mb-4 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     onChange={e => setFrom(e.target.value)} />
-//                   <input placeholder='Message'
-//                     className="block w-full rounded-md border-gray-300 shadow-sm mb-4 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     onChange={e => setMessage(e.target.value)} />
-//                   <input placeholder='Amount'
-//                     className="block w-full rounded-md border-gray-300 shadow-sm mb-4 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     onChange={e => setAmount(e.target.value)} />
-//                   <button onClick={onPay} className='bg-black text-white p-4 mt-2' >Click2Pay</button>
-//                 </Action>
-//                 <Action className="bg-white p-6 rounded-lg shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-//                   <h3 className='text-xl font-bold text-center mb-4'>Withdrawl</h1>
-//                   <input placeholder='Withdrawl From A CashTag You Own'
-//                     className="block w-full rounded-md border-gray-300 shadow-sm mb-4 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//                     onChange={e => setWithdrawl(e.target.value)} />
-//                   <button onClick={onWithdrawl} className='bg-black text-white p-4 mt-2' >Click2Withdrawl</button>
-//                 </Action>
-//                 <Action className="bg-white p-6 rounded-lg shadow-lg">
-//                   <h3 className='text-xl font-bold text-center mb-4'>Messages</h1>
-//                   <div className='w-full flex flex-wrap'>
-//                     {messages?.map((i, index) => (
-//                       <div key={index} className='overflow-auto flex w-full border flex-col items-center p-4 mb-4 bg-gray-50 rounded-lg shadow '>
-//                         <h3 className='border-b border-b-gray-200 w-full pb-2 mb-2 text-center font-semibold'>MESSAGE</h1>
-//                         <div>Sender:&nbsp;{i.sender}</div>
-//                         <div>Reciever:&nbsp;{i.receiver}</div>
-//                         <div>Message:&nbsp;{i.content}</div>
-//                       </div>
-//                     ))}
-//                   </div>
-//                 </Action>
-//               </div>
-//               : <div className='flex justify-center items-center h-60'><div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div></div>}
-//           </div>
-//         </div>
-//         : <div className='flex justify-center items-center h-screen'>
-//           <div className="text-center">
-//             <p className='text-xl font-bold'>Connect to Base GOERLI to Continue</p>
-//           </div>
-//         </div>
-//       }
-//     </div>
-//   )
-
-// }
+};
