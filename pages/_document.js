b008c5e0a3719a0a48e9488cb9eb6dd3745f3d5e
PhatStraw <kevindsimsjr@gmail.com>
2023-11-02 00:19:24 -0700

    added-contracts

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" class="h-full bg-emerald-600">
+    <Html lang="en" class="h-full">
       <Head />
-      <body class="h-full bg-emerald-600">
+      <body class="h-full">
         <Main />
         <NextScript />
       </body>
